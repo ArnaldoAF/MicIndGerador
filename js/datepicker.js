b190028caf6cf5216c66d62cc0a71db6b726087b
ArnaldoAF <arnaldo_assisferreira@outlook.com>
2018-07-31 23:16:30 -0300

    Adicionado arquivos js e css - Calendario não funciona em app vue

---
+++ js/datepicker.js
@@ -0,0 +1,10 @@
+/*
+var pickerSkeleton = new Pikaday({
+    field: document.getElementById('datepicker-skeleton'),
+    firstDay: 1,
+    minDate: new Date(2010, 0, 1),
+    maxDate: new Date(2020, 12, 31),
+    yearRange: [2010, 2020],
+    theme: 'skeleton-theme'
+  });
+  */
